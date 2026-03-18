4681dd4af68fed13f69bac60bf7f50cb55570164
Prayushstha <prayushstha1212@gmail.com>
2026-03-18 19:20:32 +0545

    fixed some shi

--- forge.config.js
+++ forge.config.js
@@ -25,45 +25,41 @@ module.exports = {
     },
   ],
   plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-    {
-      name: '@electron-forge/plugin-webpack',
-      config: {
-        mainConfig: './webpack.main.config.js',
-        renderer: {
-          config: './webpack.renderer.config.js',
-          entryPoints: [
-            {
-              html: './src/index.html',
-              js: './src/renderer.js',
-              name: 'main_window',
-              preload: {
-                js: './src/preload.js',
-              },
+  {
+    name: '@electron-forge/plugin-webpack',
+    config: {
+      mainConfig: './webpack.main.config.js',
+      renderer: {
+        config: './webpack.renderer.config.js',
+        entryPoints: [
+          {
+            html: './src/index.html',
+            js: './src/renderer.js',
+            name: 'main_window',
+            preload: {
+              js: './src/preload.js',
             },
-            {
-              html: './src/app.html',
-              js: './src/app-renderer.js',
-              name: 'app_window',
-              preload: {
-                js: './src/preload.js',
-              },
+          },
+          {
+            html: './src/app.html',
+            js: './src/app-renderer.js',
+            name: 'app_window',
+            preload: {
+              js: './src/preload.js',
             },
-          ],
-        },
+          },
+        ],
       },
     },
-    new FusesPlugin({
-      version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
-      [FuseV1Options.EnableCookieEncryption]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true,
-    }),
-  ],
+  },
+  new FusesPlugin({
+    version: FuseVersion.V1,
+    [FuseV1Options.RunAsNode]: false,
+    [FuseV1Options.EnableCookieEncryption]: true,
+    [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+    [FuseV1Options.EnableNodeCliInspectArguments]: false,
+    [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+    [FuseV1Options.OnlyLoadAppFromAsar]: true,
+  }),
+],
 };
