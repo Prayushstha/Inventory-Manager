32bec61e6b788747d88712d79f79fcddc311f179
Prayushstha <prayushstha1212@gmail.com>
2026-06-20 11:24:33 +0545

    Started Inventory page

--- src/pages/inventory.jsx
+++ src/pages/inventory.jsx
@@ -1,7 +1,12 @@
-
+import '../styles/inventory.css'
 export function InventoryPage(){
       return (
             <>
+            <div className="product-list-container">
+                  <div className="product-list">
+                        
+                  </div>
+            </div>
             </>
         )
 }
