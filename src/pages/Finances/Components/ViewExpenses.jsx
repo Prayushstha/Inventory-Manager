3b76fbf0a18646f71b079d2422955b5f2201a684
Prayushstha <prayushstha1212@gmail.com>
2026-08-25 11:23:28 +0545

    Added Expenses data for testing

--- src/pages/Finances/Components/ViewExpenses.jsx
+++ src/pages/Finances/Components/ViewExpenses.jsx
@@ -1,11 +1,33 @@
 import '../styles/viewexpenses.css'
 import { useState } from 'react';
+
+const MockExpenses = [
+  {
+    id: 123,
+    date: new Date("2026-08-03"),
+    expense: 'Imported Products',
+    totalExpense: 32000,
+  },
+   {
+    id: 124,
+    date: new Date("2026-08-04"),
+    expense: 'Taxes',
+    totalExpense: 2000,
+  },
+   {
+    id: 125,
+    date: new Date("2026-08-05"),
+    expense: 'General Expending',
+    totalExpense: 320,
+  }
+]
+
 export function ViewExpenses(){
    const [viewingBtn, setViewingBtn] = useState(1);
    return (
      <div className="view-expenses">
        <div className="expenses-top">
-         <h4>View Sales:</h4>
+         <h4>View Expenses:</h4>
          <div className="switch-btns">
            <button
              onClick={() => setViewingBtn(1)}
@@ -34,9 +56,46 @@ export function ViewExpenses(){
          </div>
        </div>
        <div className="expenses-overview-table">
-
+       {MockExpenses.length === 0 ? (
+          <div className="no-items">
+            <h1>NO EXPENSES RECORDED</h1>
+          </div>
+        ) : (
+          <ExpensesTable />
+        )}
        </div>
  
      </div>
    );
+}
+function ExpensesTable() {
+  return (
+    <div className="sales-table-container">
+      <table width={"3px"} className="sales-table">
+        <thead>
+          <tr>
+            <th>SN</th>
+            <th>Date</th>
+            <th>Expenses Info</th>
+            <th>Total Expenses</th>
+            <th className="empty-th"> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {MockExpenses.map((expense) => 
+          { return <tr key={expense.id}>
+              <td>{expense.id}</td>
+              <td>{expense.date.toLocaleDateString()}</td>
+              <td>{expense.expense}</td>
+              <td>NPR {expense.totalExpense}</td>
+              <td>
+                {" "}
+                <button className="details-btn">View Details</button>
+              </td>
+            </tr>}
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 }
