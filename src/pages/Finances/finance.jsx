0675e68c53fd507250bec1defd7ac1c359d099da
Prayushstha <prayushstha1212@gmail.com>
2026-08-26 13:30:59 +0545

    Fixed CSS issues for importing products

--- src/pages/Finances/finance.jsx
+++ src/pages/Finances/finance.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { FinanceHeader } from "./Components/FinanceHeader";
 import './styles/header.css'
+import './styles/dialogs.css'
 import { ViewSales } from "./Components/ViewSales";
 import { ViewExpenses } from "./Components/ViewExpenses";
 import { NetPosition } from "./Components/NetPosition";
